59b2bc07ce3d8ddaa904e9334a87abd6129229a0
AriStWeb <sole21008@gmail.com>
2023-05-15 11:27:27 -0300

    limpiando

--- portfolio/src/main/java/com/argentinaprograma/miPortfolio/Controller/BannerController.java
+++ portfolio/src/main/java/com/argentinaprograma/miPortfolio/Controller/BannerController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200",maxAge = 3600)
+@CrossOrigin
 @RestController
 public class BannerController {
     
--- portfolio/src/main/java/com/argentinaprograma/miPortfolio/Repository/IUsuarioRepository.java
+++ portfolio/src/main/java/com/argentinaprograma/miPortfolio/Repository/IUsuarioRepository.java
@@ -2,12 +2,10 @@
 package com.argentinaprograma.miPortfolio.Repository;
 
 import com.argentinaprograma.miPortfolio.Model.Usuario;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IUsuarioRepository extends JpaRepository <Usuario,Long> {
-    
-    Optional<Usuario> findOneByEmail(final String email);
+
 }
